2bc53c52237aa8f66de76314f81cf8b35804a171
Marco Bonafede <marcobonafede@hotmail.com>
2023-10-20 20:41:00 -0300

    Logica terminada -falta "jugar"

--- entrega1.py
+++ entrega1.py
@@ -1,10 +1,10 @@
 from simpleai.search import SearchProblem
 from simpleai.search.traditional import breadth_first, depth_first, limited_depth_first, iterative_limited_depth_first, \
     uniform_cost, greedy, astar
-from simpleai.search.viewers import WebViewer, ConsoleViewer, BaseViewer
+from simpleai.search.viewers import WebViewer, BaseViewer
 import math
 
-''' 
+'''
 secuencia = jugar(
     filas=5,
     columnas=5,
@@ -20,18 +20,20 @@ secuencia = jugar(
         ...
     ],
     pieza_sacar="pieza_roja",
-) 
+)
 '''
 
-INITIAL_STATE = (("pieza_verde", 0, ((0, 0), (0, 1), (0, 2))),
-                 ("pieza_roja", 0, ((1, 0), (2, 0))),
-                 ("pieza_azul", 2, ((1, 0), (1, 1), (2, 1)))
+INITIAL_STATE = (
+        ("pieza_verde", 1, ((0, 0), (0, 1), (1, 1))),
+        ("pieza_roja", 2, ((0, 0), (0, 1))),
+        ("pieza_azul", 0, ((2, 2), (2, 3), (3, 2))),
+        ("pieza_amarilla", 2, ((0, 2), (1, 2)))
 )
 
 PISOS = 3
-FILAS = 5
-COLUMNAS = 5
-SALIDA = (0, 3, 1)
+FILAS = 4
+COLUMNAS = 4
+SALIDA = (0, 3, 3)
 PIEZA_SACAR = "pieza_roja"
 
 
@@ -39,18 +41,20 @@ def obtener_pieza_objetivo(state, pieza_a_retornar=PIEZA_SACAR):
     return list([pieza for pieza in state if pieza[0] == pieza_a_retornar][0])
 
 
-def obtener_piezas_mismo_piso(state, piso):
-    return list([pieza for pieza in state if pieza[1] == piso])
+def obtener_piezas_mismo_piso(state, pieza_a_mover):
+    return list([pieza for pieza in state if (pieza[1] == pieza_a_mover[1]) & (pieza[0] != pieza_a_mover[0])])
 
 
 def es_movimiento_valido(state, pieza_a_mover):
     nombre_pieza, piso_pieza, posiciones_partes = pieza_a_mover
 
-    piezas_mismo_piso = obtener_piezas_mismo_piso(state, piso_pieza)
+    piezas_mismo_piso = obtener_piezas_mismo_piso(state, pieza_a_mover)
 
+    # Control de que no se vaya de los limites verticales
     if piso_pieza < 0 or piso_pieza >= PISOS:
         return False
 
+    # Limites laterales
     for coordenada in posiciones_partes:
         if not (0 <= coordenada[0] < FILAS):
             return False
@@ -58,9 +62,11 @@ def es_movimiento_valido(state, pieza_a_mover):
         if not (0 <= coordenada[1] < COLUMNAS):
             return False
 
+    # Colisiones
     for pieza in piezas_mismo_piso:
         if bool(set(pieza[2]) & set(posiciones_partes)):
             return False
+
     return True
 
 
@@ -72,10 +78,10 @@ def calcular_nueva_posicion(pieza_a_mover, movimiento):
     mod_piso = 0
 
     if movimiento == "arriba":
-        mod_fila = 1
+        mod_fila = -1
 
     if movimiento == "abajo":
-        mod_fila = -1
+        mod_fila = 1
 
     if movimiento == "derecha":
         mod_columna = 1
@@ -91,11 +97,13 @@ def calcular_nueva_posicion(pieza_a_mover, movimiento):
 
     piso_pieza += mod_piso
 
-    for coordenada in list(map(list, posiciones_partes)):
+    lista_coordenadas = list(map(list, posiciones_partes))
+
+    for coordenada in lista_coordenadas:
         coordenada[0] += mod_fila
         coordenada[1] += mod_columna
 
-    return nombre_pieza, piso_pieza, posiciones_partes
+    return nombre_pieza, piso_pieza, tuple(map(tuple, lista_coordenadas))
 
 
 class RushHourProblem(SearchProblem):
@@ -175,9 +183,9 @@ def jugar(self, filas, columnas, pisos, salida, piezas, pieza_sacar):
 if __name__ == '__main__':
     my_problem = RushHourProblem(INITIAL_STATE)
 
-    v = WebViewer()
+   #v = WebViewer()
 
-    result = astar(my_problem, viewer=v)
+    result = astar(my_problem)# viewer=v)
 
     for action, state in result.path():
         print("A:", action)
